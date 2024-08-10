0862fd447642f8548e6637f3806e5c9af48d4c0e
root <root@LAPTOP-SM14D7L7>
2024-08-11 06:07:05 +0800

    solidity style guide

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,25 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.19;
 
