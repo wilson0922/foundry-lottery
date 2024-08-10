ae8ca93cae4bf14855f21b0792d3a33504ab480f
wilson.lin <tree70373@gmail.com>
2024-08-11 06:51:18 +0800

    create custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,12 +30,22 @@ pragma solidity ^0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    // Errors
+    error Raffle_NotEnoughEth();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee){
         i_entranceFee=entranceFee;
     }
-    function  enterRaffle() public payable{}
+    function  enterRaffle() public payable{
+        // require(msg.value<i_entranceFee,'Not enough ETH sent');
+        // require(msg.value<i_entranceFee,Raffle_NotEnoughEth()); in v0.8.26 and need IR to compile
+        
+        if(msg.value<i_entranceFee){
+            revert Raffle_NotEnoughEth();
+        }
+    }
 
     function  pickWinner() public {}
 
