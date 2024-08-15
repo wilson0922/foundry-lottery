bb22e0fcea9226212106e39ee2e375d49c760cd4
wilson.lin <tree70373@gmail.com>
2024-08-15 10:15:45 +0800

    deploy mock Chainlink VRF

--- scriipt/HelperConfig.s.sol
+++ scriipt/HelperConfig.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "lib/forge-std/src/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstance{
+    // VRF mock values
+    uint96 public MOCK_BASE_FEE=0.25 ether;
+    uint96 public MOCK_GAS_PRICE_LINK=1e9;
+    // LINK / ETH price
+    int256 public MOCK_WEI_PER_UNIT_LINK=4e15;
+
     uint256 public constant SEPOLIA_CHAIN_ID=11155111;
     uint256 public constant LOCAL_CHAIN_ID=31337;
-
 }
 
 contract HelperConfig is Script, CodeConstance {
@@ -32,7 +38,7 @@ contract HelperConfig is Script, CodeConstance {
         if(networkConfigs[chainId].vrfCoordinator!=address(0)){
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
-            getOrCreateAnvilEthConfig();
+            return getOrCreateAnvilEthConfig();
         }else{
             revert HelperConfig__InvalidChainId();
         }
@@ -53,6 +59,25 @@ contract HelperConfig is Script, CodeConstance {
         if(localNetworkConfig.vrfCoordinator!=address(0)){
             return localNetworkConfig;
         }
+
+        // Deploy mocks
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock=new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE,MOCK_GAS_PRICE_LINK,MOCK_WEI_PER_UNIT_LINK); 
+        vm.stopBroadcast();
+
+        localNetworkConfig=NetworkConfig({
+            entranceFee:0.01 ether,
+            interval:30, // 30 sec
+            vrfCoordinator: address(vrfCoordinatorMock),
+            // doesn't matter
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId:0, // might have to fix this
+            callbackGasLimit:500000 // 500,000gas
+        });
+
+        return localNetworkConfig;
+
+
     }
 
     
